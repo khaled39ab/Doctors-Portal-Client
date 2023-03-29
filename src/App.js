45e91cc659ac7392c456aad1d7f84362da35ec79
khaled39ab <khaled39ab@gmail.com>
2023-03-29 23:38:52 +0600

    setup files completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Router/Routes/Routes';
 
 function App() {
   return (
     <div>
-      <button className="btn btn-active">Button</button>
-      <button className="btn btn-active btn-primary">Button</button>
-      <button className="btn btn-active btn-secondary">Button</button>
-      <button className="btn btn-active btn-accent">Button</button>
-      <button className="btn btn-active btn-ghost">Button</button>
-      <button className="btn btn-active btn-link">Button</button>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/Router/Routes/Routes.jsx
+++ src/Router/Routes/Routes.jsx
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../../Layout/Main';
+import Login from '../../Pages/Authentication/Login/Login';
+import Register from '../../Pages/Authentication/Register/Register';
+import Home from '../../Pages/Home/Home/Home';
 
  const router = createBrowserRouter([
         {
@@ -7,7 +10,16 @@ import Main from '../../Layout/Main';
             element: <Main />,
             children: [
                 {
-
+                    path: '/',
+                    element: <Home />
+                },
+                {
+                    path: '/login',
+                    element: <Login />
+                },
+                {
+                    path: '/register',
+                    element: <Register />
                 }
             ]
         }
