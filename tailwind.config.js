f93dc339c190d456ab0864900734a11913095b6e
khaled39ab <khaled39ab@gmail.com>
2023-03-29 23:57:35 +0600

    customized themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        doctorsPortalTheme: {
+
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#181A2A",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
