a7146284896cd96fc30bd594b3554c7140b10d5b
khaled39ab <khaled39ab@gmail.com>
2023-03-30 22:43:50 +0600

    banner section done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -4,13 +4,15 @@ import chair from '../../../assets/images/chair.png';
 
 const Banner = () => {
     return (
-        <div className="hero banner">
+        <div className="hero banner my-10">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} alt='chair' className="w-1/2 rounded-lg shadow-2xl" />
                 <div>
                     <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <p className="py-6 text-justify">
+                        Have you ever looked in the mirror and noticed problems with your smile? Do you have a variety of issues that you feel like are beyond repair? They are not! You can transform your smile with dental veneers. Contact our office today to schedule your consultation and learn more. Call us or book a new patient appointment today.
+                    </p>
+                    <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary">Get Started</button>
                 </div>
             </div>
         </div>
