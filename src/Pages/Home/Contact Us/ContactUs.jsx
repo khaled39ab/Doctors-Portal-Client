fe10b63bb161934644a772744e791e572ad0badb
khaled39ab <khaled39ab@gmail.com>
2023-04-01 23:35:43 +0600

    contact us section done

--- src/Pages/Home/Contact Us/ContactUs.jsx
+++ src/Pages/Home/Contact Us/ContactUs.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import appointment from '../../../assets/images/appointment.png';
 
 const ContactUs = () => {
     return (
-        <div>
-            
+        <div
+            style={
+                { background: `url(${appointment})` }
+            }
+            className='my-10'
+        >
+            <div className='text-center pt-10'>
+                <h2 className='text-primary text-xl font-bold'>Contact Us</h2>
+                <h1 className='text-2xl md:text-3xl lg:text-4xl text-white'>Stay Connect With us</h1>
+            </div>
+            <form className='mt-10 pb-16 text-center'>
+                <input type="text" placeholder="Email Address" className="input w-full max-w-xs" /> <br />
+                <input type="text" placeholder="Subject" className="input w-full max-w-xs my-3" /> <br />
+                <textarea className="textarea w-full max-w-xs textarea-lg" placeholder="Your Message"></textarea> <br />
+
+                <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white mt-5 px-6">Submit</button>
+            </form>
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link>Home</Link></li>
+                        <li><Link to={'/'}>Home</Link></li>
                         <li><Link>About</Link></li>
                         <li><Link>Appointment</Link></li>
                         <li><Link to={'/reviews'}>Reviews</Link></li>
@@ -18,11 +18,11 @@ const Navbar = () => {
                         <li><Link>Login</Link></li>
                     </ul>
                 </div>
-                <Link className="btn btn-ghost normal-case text-xl">Doctors Portal</Link>
+                <Link to={'/'} className="btn btn-ghost normal-case text-xl">Doctors Portal</Link>
             </div>
             <div className="navbar-end hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link>Home</Link></li>
+                    <li><Link to={'/'}>Home</Link></li>
                     <li><Link>About</Link></li>
                     <li><Link>Appointment</Link></li>
                     <li><Link to={'/reviews'}>Reviews</Link></li>
