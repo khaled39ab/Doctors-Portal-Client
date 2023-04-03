6063812e9a4dde852a6d4085b4cb4c2e47455665
khaled39ab <khaled39ab@gmail.com>
2023-04-03 11:03:42 +0600

    appointment calendar displayed successfully

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.jsx
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../../../assets/images/chair.png';
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
 
 const AppointmentBanner = () => {
+
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
     return (
         <header>
             <div className="hero">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img alt='chair' src={chair} className="lg:w-1/2 rounded-lg shadow-2xl" />
-                    <div>
-                        
+                    <img alt='chair' src={chair} className="lg:w-2/5 rounded-lg shadow-2xl" />
+                    <div className='mr-10'>
+                        <DayPicker
+                            mode='single'
+                            selected={selectedDate}
+                            onSelect={setSelectedDate}
+                        />
+
+                        <p>You picked {format(selectedDate, 'PPP')}.</p>;
                     </div>
                 </div>
             </div>
