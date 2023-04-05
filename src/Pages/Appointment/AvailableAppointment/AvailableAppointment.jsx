c3d445229009430eb3589c744b702d3b92552321
khaled39ab <khaled39ab@gmail.com>
2023-04-05 23:17:19 +0600

    create available time with fake data

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.jsx
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.jsx
@@ -1,11 +1,30 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AvailableOption from './AvailableOption';
 
 const AvailableAppointment = ({ selectedDate }) => {
+
+    const [availableAppointment, setAvailableAppointment] = useState([]);
+
+    useEffect(() => {
+        fetch('AvailableAppointment.json')
+            .then(res => res.json())
+            .then(data => setAvailableAppointment(data))
+    }, []);
+
+
     return (
         <section className='my-12'>
             <div className='text-center text-secondary font-semibold'>
                 <p>Available Service on {format(selectedDate, 'PPP')}.</p>;
+            </div>
+            <div>
+                {
+                    availableAppointment.map(availableOption => <AvailableOption
+                        key={availableOption._id}
+                        availableOption={availableOption}
+                    ></AvailableOption>)
+                }
             </div>
         </section>
     );
---
+++ src/Pages/Appointment/AvailableAppointment/AvailableOption.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AvailableOption = ({ availableOption }) => {
+
+    const {name, slots} = availableOption;
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AvailableOption;
