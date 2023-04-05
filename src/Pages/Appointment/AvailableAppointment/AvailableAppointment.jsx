4fa554928f5d8b8f430138995585ff7cc249b3b1
khaled39ab <khaled39ab@gmail.com>
2023-04-05 23:37:10 +0600

    display available seervice done

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.jsx
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.jsx
@@ -15,10 +15,11 @@ const AvailableAppointment = ({ selectedDate }) => {
 
     return (
         <section className='my-12'>
-            <div className='text-center text-secondary font-semibold'>
-                <p>Available Service on {format(selectedDate, 'PPP')}.</p>;
+            <div className='text-center'>
+                <p className='text-secondary font-semibold text-2xl'>Available Service on {format(selectedDate, 'PPP')}.</p>
+                <p className='text-accent font-medium opacity-60 text-xl mt-3'>Please select a service.</p>
             </div>
-            <div>
+            <div  className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8'>
                 {
                     availableAppointment.map(availableOption => <AvailableOption
                         key={availableOption._id}
--- src/Pages/Appointment/AvailableAppointment/AvailableOption.jsx
+++ src/Pages/Appointment/AvailableAppointment/AvailableOption.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const AvailableOption = ({ availableOption }) => {
 
-    const {name, slots} = availableOption;
+    const { name, slots } = availableOption;
 
     return (
-        <div>
-
+        <div className="card mx-10 shadow-xl cursor-pointer">
+            <div className="card-body text-center mx-auto">
+                <h2 className="card-title text-secondary ">{name}</h2>
+            </div>
         </div>
     );
 };
