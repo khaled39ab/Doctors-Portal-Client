2875a24e535e7ae9873acce00d675e77ea366078
khaled39ab <khaled39ab@gmail.com>
2023-04-07 23:47:06 +0600

    styled login page

--- src/Pages/Authentication/Login/Login.jsx
+++ src/Pages/Authentication/Login/Login.jsx
@@ -10,17 +10,22 @@ const Login = () => {
     return (
         <div className='h-[500px] flex justify-center items-center'>
             <div className=''>
-                <h2 className='text-3xl font-semibold'>Login</h2>
+                <h2 className='text-3xl font-semibold text-center mb-5'>Login</h2>
                 <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
-                    <input {...register("firstName")} placeholder="First name" />
-                    <select {...register("category", { required: true })}>
-                        <option value="">Select...</option>
-                        <option value="A">Option A</option>
-                        <option value="B">Option B</option>
-                    </select>
-                    <textarea {...register("aboutYou")} placeholder="About you" />
-                    <p>{data}</p>
-                    <input type="submit" />
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text text-lg">Email</span>
+                        </label>
+                        <input type="email" {...register("email")} placeholder="Enter Your Email" className="input input-bordered w-full" />
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text text-lg">Password</span>
+                        </label>
+                        <input type="password" {...register("password")} placeholder="Enter Your Password" className="input input-bordered w-full" />
+                    </div>
+                    <br />
+                    <input type="submit" className="btn btn-accent w-full" />
                 </form>
             </div>
         </div>
