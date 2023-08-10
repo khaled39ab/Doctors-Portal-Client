4171ef1871c1445a58c00dd7d1073224bd1c7b96
khaled39ab <khaled39ab@gmail.com>
2023-08-10 16:03:05 +0600

    start again the project

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.jsx
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.jsx
@@ -4,9 +4,10 @@ import { DayPicker } from 'react-day-picker';
 
 const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
 
+
     return (
         <header>
-            <div className="hero">
+            <div className="hero banner py-20">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img alt='chair' src={chair} className="lg:w-1/2 rounded-lg shadow-2xl" />
                     <div className='mr-10 shadow-xl'>
