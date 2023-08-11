c101cc68516778bd2a50282ba8bc79c751ab8873
khaled39ab <khaled39ab@gmail.com>
2023-08-12 00:04:21 +0600

    make loading component

--- src/Router/RequireAuth/RequireAuth.jsx
+++ src/Router/RequireAuth/RequireAuth.jsx
@@ -1,17 +1,14 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import Loading from '../../Pages/Shared/Loading/Loading';
 
 const RequireAuth = ({children}) => {
     const { user, isLoading } = useContext(AuthContext);
     const location = useLocation();
 
     if (isLoading) {
-        return<div class="flex justify-center items-center h-screen">
-            <div class="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-purple-400 via-blue-500 to-red-400 ">
-                <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-gray-200 rounded-full border-2 border-white"></div>
-            </div>
-        </div>
+        return <Loading />
     }
 
     if (!user) {
