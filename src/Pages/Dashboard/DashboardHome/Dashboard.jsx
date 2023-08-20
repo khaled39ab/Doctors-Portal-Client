c9d0e949745a3a44fa0623bd9e8ef7bd654f22dd
khaled39ab <khaled39ab@gmail.com>
2023-08-20 21:32:28 +0600

    create dropdown dashboard component

--- src/Pages/Dashboard/DashboardHome/Dashboard.jsx
+++ src/Pages/Dashboard/DashboardHome/Dashboard.jsx
@@ -4,15 +4,14 @@ import { Link, Outlet } from 'react-router-dom';
 const Dashboard = () => {
     return (
         <div className="drawer drawer-mobile">
-            <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
+            <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content ">
                 <h2 className='text-4xl text-purple-600 font-bold'>Dashboard</h2>
                 <Outlet />
-                <label htmlFor="dashboard-drawer" className="btn btn-primary drawer-button lg:hidden">Open Dashboard</label>
 
             </div>
             <div className="drawer-side">
-                <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+                <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
                     <li><Link to='/dashboard/my-appointment'>My Appointment</Link></li>
                     <li><Link to='/dashboard/my-review'>My Review</Link></li>
