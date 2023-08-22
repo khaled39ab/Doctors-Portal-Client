8888f1eb029bb4cc46c507aec8d63c84e54315e6
khaled39ab <khaled39ab@gmail.com>
2023-08-22 15:30:30 +0600

    make realistic the dashboard component

--- src/Pages/Dashboard/DashboardHome/Dashboard.jsx
+++ src/Pages/Dashboard/DashboardHome/Dashboard.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 
 const Dashboard = () => {
+    const { user } = useContext(AuthContext);
     return (
         <div className="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content ">
-                <h2 className='text-4xl text-purple-600 font-bold'>Dashboard</h2>
-                <Outlet />
+                <h2 className='text-5xl uppercase text-purple-600 font-bold underline underline-offset-8'>Dashboard</h2>
+
+                <div className="flex flex-col w-full border-opacity-50">
+                    <div className=' flex flex-col items-center justify-center mt-8'>
+                        <h2 className='text-4xl text-blue-500 font-bold'>Hello, {user.displayName}!</h2>
+                        <h3 className='text-2xl text-fuchsia-700 font-bold'>Email: <span className='text-2xl text-red-500'>{user.email}</span></h3>
+                        <h3 className='text-2xl text-green-500 font-bold'>Welcome to Dashboard</h3>
+                    </div>
+
+                    <div className="divider"></div>
+
+                    <div className="grid card bg-base-300 rounded-box place-items-center p-5">
+                        <Outlet />
+                    </div>
+                </div>
+
+
+
 
             </div>
             <div className="drawer-side">
--- src/Pages/Dashboard/MyAppointment/MyAppointment.jsx
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.jsx
@@ -1,21 +1,68 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
+import Loading from '../../Shared/Loading/Loading';
 
 const MyAppointment = () => {
-    const { user } = useContext(AuthContext);
+    const { user, isLoading } = useContext(AuthContext);
     const [myApp, setMyApp] = useState([]);
 
     useEffect(() => {
+        if (isLoading) {
+            return <Loading />
+        }
+        fetch(`http://localhost:4000/bookings?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => setMyApp(data))
+
+    }, [user.email, isLoading]);
+    
+    /* useEffect(() => {
         if (user) {
             fetch(`http://localhost:4000/bookings?email=${user.email}`)
                 .then(res => res.json())
                 .then(data => setMyApp(data))
         }
-    }, [user])
+    }, [user]) */
 
     return (
         <div>
-            <h1>My Appointment {myApp.length}</h1>
+            <h1>Total Appointment {myApp.length}</h1>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                        {/* row 2 */}
+                        <tr className="hover">
+                            <th>2</th>
+                            <td>Hart Hagerty</td>
+                            <td>Desktop Support Technician</td>
+                            <td>Purple</td>
+                        </tr>
+                        {/* row 3 */}
+                        <tr>
+                            <th>3</th>
+                            <td>Brice Swyre</td>
+                            <td>Tax Accountant</td>
+                            <td>Red</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
