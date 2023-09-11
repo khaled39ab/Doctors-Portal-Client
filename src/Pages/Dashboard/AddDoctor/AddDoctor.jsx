e9164bec5eb0884f8f8e6b2e652e47efcd0bac30
khaled39ab <khaled39ab@gmail.com>
2023-09-12 00:27:08 +0600

    complete add doctor form

--- src/Pages/Dashboard/AddDoctor/AddDoctor.jsx
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.jsx
@@ -15,6 +15,7 @@ const AddDoctor = () => {
             <h2 className='text-3xl font-bold text-green-600 underline uppercase'>_ Add A Doctor _</h2>
 
             <form onSubmit={handleSubmit(handleSignUp)}>
+
                 <div className="form-control w-full">
                     <label className="label">
                         <span className="label-text text-lg">Name</span>
@@ -27,6 +28,7 @@ const AddDoctor = () => {
                     />
                     {errors.name && <p className='text-red-600'>{errors.name?.message}</p>}
                 </div>
+
                 <div className="form-control w-full">
                     <label className="label">
                         <span className="label-text text-lg">Email</span>
@@ -41,20 +43,37 @@ const AddDoctor = () => {
                         errors.email && <p className='text-red-600'>{errors.email?.message}</p>
                     }
                 </div>
+
                 <div className="form-control w-full">
                     <label className="label">
                         <span className="label-text text-lg">Specialty</span>
                     </label>
+                    <select
+                        {...register("specialty", { required: "Specialty is required" })}
+                        className="select select-primary w-full max-w-xs"
+                    >
+                        {
+                            specialty.map(special => <option
+                                key={special._id}
+                            >{special.name}</option>)
+                        }
+                    </select>
+                </div>
+
+                <div className="form-control w-full">
+                    <label className="label">
+                        <span className="label-text text-lg">Photo</span>
+                    </label>
                     <input
-                        type="text"
-                        {...register("specialty", {
-                            required: "Specialty is required"
-                        })}
-                        placeholder="Enter Specialty"
+                        type="file"
+                        {...register("image", { required: "Email Address is required" })}
                         className="input input-bordered w-full"
                     />
-                    {errors.specialty && <p className='text-red-600'>{errors.specialty?.message}</p>}
+                    {
+                        errors.image && <p className='text-red-600'>{errors.image?.message}</p>
+                    }
                 </div>
+
                 <input type="submit" value='Add A Doctor' className="btn btn-accent w-full text-white mt-3" />
             </form>
         </div>
